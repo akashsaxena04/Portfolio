bf1bac2c4f171a7680e5526779d04050b7d135b2
akashsaxena04 <akash.saxena1404@gmail.com>
2026-03-23 18:27:04 +0530

    changes made to contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
               </div>
               <div className="contact-item mt-1">
                 <span className="contact-label">LinkedIn</span>
-                <a href="https://linkedin.com/in/akash04" target="_blank" rel="noopener noreferrer" className="contact-link glow-text">
+                <a href="https://linkedin.com/in/akash-saxena04" target="_blank" rel="noopener noreferrer" className="contact-link glow-text">
                   linkedin.com/in/akash04
                 </a>
               </div>
